ebc6675835b25a7c849ace1d2834cfd6d49bcbf7
Augusto Cezar <augustocezaa@gmail.com>
2025-02-04 17:15:56 -0300

    Add project files.

---
+++ CodeWars/CodeWars/SempleAssemblerInterpreter.cs
@@ -0,0 +1,79 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CodeWars.CodeWars;
+
+public static class SempleAssemblerInterpreter
+{
+    public static Dictionary<string, int> Interpret(string[] program)
+    {
+
+        var result = new Dictionary<string, int>();
+        var instructionCounter = 0;
+
+        do
+        {
+            var instruction = program[instructionCounter].Split(' ');
+
+            if (instruction[0] == "mov")
+            {
+                if(Int32.TryParse(instruction[2], out int convertedValue))
+                {
+                    if (!result.ContainsKey(instruction[1]))
+                    {
+                        result.Add(instruction[1], convertedValue);
+                    }
+                    else
+                    {
+                        result[instruction[1]] = convertedValue;
+                    }
+                }
+                else
+                {
+                    
+                    result.TryGetValue(instruction[2], out var value);
+                    if (!result.ContainsKey(instruction[1]))
+                    {
+                        result.Add(instruction[1], value);
+                    }
+                    else
+                    {
+                        result[instruction[1]] = value;
+                    }
+                }
+                
+                instructionCounter++;
+            } else if (instruction[0] == "inc")
+            {
+                result.TryGetValue(instruction[1], out var value);
+                result[instruction[1]] = value + 1;
+                instructionCounter++;
+            } else if (instruction[0] == "dec")
+            {
+                result.TryGetValue(instruction[1], out var value);
+                result[instruction[1]] = value - 1;
+                instructionCounter++;
+            }
+            else if (instruction[0] == "jnz")
+            {
+                if (!int.TryParse(instruction[1], out int value))
+                {
+                    value = result.GetValueOrDefault(instruction[1], 0);
+                }
+                
+                if (!int.TryParse(instruction[2], out int jump))
+                {
+                    jump = result.GetValueOrDefault(instruction[2], 0);
+                }
+
+                if (value != 0)
+                    instructionCounter += jump;
+                else
+                    instructionCounter++;
+            }
+        }while(instructionCounter < program.Length);
+
+        return result;
+
+    }
+    
+}
---
+++ CodeWars/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.Json.Serialization;
+using CodeWars.CodeWars;
+
+var result = SempleAssemblerInterpreter.Interpret([
+    "mov a 5",
+    "inc a",
+    "dec a",
+    "dec a",
+    "jnz a -1",
+    "inc a",
+    "mov a -10",
+    "mov b a",
+    "inc a",
+    "dec b",
+    "jnz a -2",
+    "mov a 1",
+    "mov b 1",
+    "mov c 0",
+    "mov d 26",
+    "jnz c 2",
+    "jnz 1 5",
+    "mov c 7",
+    "inc d",
+    "dec c",
+    "jnz c -2",
+    "mov c a",
+    "inc a",
+    "dec b",
+    "jnz b -2",
+    "mov b c",
+    "dec d",
+    "jnz d -6",
+    "mov c 18",
+    "mov d 11",
+    "inc a",
+    "dec d",
+    "jnz d -2",
+    "dec c",
+    "jnz c -5"
+]);
+
+foreach (KeyValuePair<string, int> kvp in result)
+{
+    //textBox3.Text += ("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+    Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+}
