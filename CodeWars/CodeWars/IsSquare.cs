2250afe0707bc52220a3b3781b4990ecb631eef3
Augusto Cezar <augustocezaa@gmail.com>
2025-02-04 17:37:01 -0300

    IsSquare

---
+++ CodeWars/CodeWars/IsSquare.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeWars.CodeWars;
+
+public static class IsSquare
+{
+    public static bool Execute(int instruction)
+    {
+        var sqrt = Math.Sqrt(instruction);
+        return (sqrt % 1) == 0;
+    }
+}
